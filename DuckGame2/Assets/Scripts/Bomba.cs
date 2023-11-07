4cff8de6a05b5c6917cc52d6bbd87ccce4a83895
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2023-11-07 21:15:17 +0100

    Bombas
    Script básico de bombas hecho

--- DuckGame2/Assets/Scripts/Bomba.cs
+++ DuckGame2/Assets/Scripts/Bomba.cs
@@ -6,18 +6,49 @@ public class Bomba : MonoBehaviour
 {
     [Header("Atributos")]
     [SerializeField] private float temporizador;
+    [SerializeField] private bool hasExploded = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Countdown();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(hasExploded);
+        if (hasExploded)
+        {
+            Explode();
+        }
     }
 
+    /// <summary>
+    /// Corutina que usa el temporizador de la bomba como los segundos que tarda en explotar
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CountdownRoutine()
+    {
+        yield return new WaitForSeconds(temporizador);
+        hasExploded = true;
+    }
+
+    /// <summary>
+    /// Inicia la corutina de explosión
+    /// </summary>
+    private void Countdown()
+    {
+        StartCoroutine(CountdownRoutine());
+    }
+
+    /// <summary>
+    /// Explota la bomba
+    /// </summary>
+    private void Explode()
+    {
+        Destroy(gameObject);
+        // Aquí irían partículas de explosión y efectos de sonido
+    }
 
 }
